06545f25e3e4ba047a1f9f4ee8e0d99ca2a39a24
Goonie19 <strawhatgoonie@gmail.com>
2019-12-16 12:26:17 +0100

    chore: added static data to show while back is not working

--- src/views/index.ts
+++ src/views/index.ts
@@ -2,6 +2,5 @@ import Login from './Login'
 import Home from './Home'
 import Election from './Election'
 import Census from './Census'
-import ExamplePDFViewer from './PdfViewer'
 
-export { Login, Home, Election, Census, ExamplePDFViewer }
+export { Login, Home, Election, Census }
