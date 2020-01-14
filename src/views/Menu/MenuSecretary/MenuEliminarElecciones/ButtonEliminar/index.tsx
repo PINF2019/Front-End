00ca37817e4b261d3ec77565d7b320f6b11941c6
Marta <martarendon180497@gmail.com>
2020-01-14 20:35:11 +0100

    commit hecho guapo

--- src/views/Menu/MenuSecretary/MenuEliminarElecciones/ButtonEliminar/index.tsx
+++ src/views/Menu/MenuSecretary/MenuEliminarElecciones/ButtonEliminar/index.tsx
@@ -1,59 +1,59 @@
-import { Button, Icon, Row, Typography } from 'antd'
-import React from 'react'
-import { useHistory } from 'react-router-dom'
+import React from "react";
+import { Row, Col, Button, Icon, Typography } from "antd";
+import { useParams, useHistory } from "react-router-dom";
+const { Text } = Typography;
 
-const { Text } = Typography
 
 type Props = {
-  name: string
-  id: string
-  href: string
-}
+  name: string;
+  id: string;
+  href: string;
+};
 
 const ButtonEliminar = (props: Props) => {
-  const history = useHistory()
+  const history = useHistory();
 
   return (
     <>
       <Button
-        // href={props.url}
+        //href={props.url}
         style={{
-          backgroundColor: '#F0F0F0',
-          marginTop: '2%',
-          minWidth: '32%',
-          minHeight: '63px',
-          borderRadius: '12px',
-          boxShadow: '0px 3px 3px grey',
+          backgroundColor: "#F0F0F0",
+          marginTop: "2%",
+          minWidth: "32%",
+          minHeight: "63px",
+          borderRadius: "12px",
+          boxShadow: "0px 3px 3px grey"
         }}
         onClick={() => history.push(`${props.href}/${props.id}`)}
       >
         <Row
           style={{
-            width: '100%',
-            height: '100%',
-            display: 'flex',
-            alignContent: 'space-between',
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            alignContent: "space-between"
           }}
         >
-          <Text strong style={{ marginTop: 'auto', marginBottom: 'auto' }}>
+          <Text strong style={{ marginTop: "auto", marginBottom: "auto" }}>
             {props.name}
           </Text>
 
           <Icon
             type="delete"
             style={{
-              marginTop: '1.1%',
-              verticalAlign: 'middle',
-              fontSize: '50px',
-              color: '#FFA500',
-              marginLeft: 'auto',
+              marginTop: "1.1%",
+              verticalAlign: "middle",
+              fontSize: "50px",
+              color: "#FFA500",
+              marginLeft: "auto"
             }}
           />
         </Row>
       </Button>
 
-      <br />
+      <br></br>
     </>
-  )
-}
-export default ButtonEliminar
+  );
+};
+export default ButtonEliminar;
