48c82f413de607eda7e310939cd72efe06c58972
Carmen <dece279@gmail.com>
2020-01-14 10:36:20 +0100

    chore: votacion funcional

--- src/views/Secretary/ProcesoElectoral/index.tsx
+++ src/views/Secretary/ProcesoElectoral/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
+import './index.less'
 import {
   Card,
   Button,
@@ -8,7 +9,11 @@ import {
   Select,
   InputNumber,
   DatePicker,
-  Transfer
+  Transfer,
+  Input,
+  Tooltip,
+  Col,
+  Row
 } from 'antd'
 import Selector from './select';
 //import { useElectionNameQuery } from '@Generated/hooks'
@@ -62,6 +67,7 @@ const CrearEleccion = () => {
   //const { data, error } = useElectionNameQuery()
   //if (data) {
   return (
+    <Col>
     <body>
       <Text strong style={{ fontSize: '25px' }}>
         Crear elección
@@ -94,7 +100,7 @@ const CrearEleccion = () => {
               max={10}
               placeholder="Número máximo de votaciones"
               onChange={numerovotaciones}
-            />
+              />
             <br></br>
             <br></br>
             <Text style={{ fontSize: '20px' }}>Duración:</Text>
@@ -102,19 +108,94 @@ const CrearEleccion = () => {
             <RangePicker
               disabledDate={comprobarinicio}
               onChange={changefecha}
-            />
+              />
             <br></br>
             <br></br>
             <Button style={{ background: '#206489',color:'#FFFFFF' }}>Enviar</Button>
             <Button style={{ background: '#FFA500' }}>Cancelar</Button>
           </form>
         </TabPane>
+
+
         <TabPane
-          tab="Elección de cargos unipersonales"
-          disabled
-          key="2"></TabPane>
+          tab="Elección de cargos unipersonales" key="2">
+          <Col>
+          
+            <Card className = "card2">
+                <Text style={{ fontSize: '20px' }}>Título de la Elección</Text>
+           </Card>
+
+            <Input  placeholder="Título" />
+            <Card className = "card2">
+                <Text style={{ fontSize: '20px' }}>Candidatos</Text>
+            </Card>
+            <Selector/>
+
+            <Card className = "card2">
+                    <Text style={{ fontSize: '20px' }}>Ponderación</Text>
+            </Card>
+                <Row
+              type="flex"
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                marginTop: '10%',
+                fontSize: '15px',
+                flex: '1'
+              }}
+            >
+          <Col style={{ width: '40%', marginLeft: '10%' }}>
+            <Row style={{ paddingBottom: '10px' }}>
+              <Text strong> Profesores Doctores con Vinculación Permanente (PDVP)</Text>
+            </Row>
+            <Row style={{ paddingBottom: '10px' }}>
+              <Text strong> Profesores No Doctores con Vinculación Permanente (PNDVP)</Text>
+            </Row>
+            <Row style={{ paddingBottom: '10px' }}>
+              <Text strong>  PDI Doctores sin vinculación Permanente (PDINVP) <br></br></Text>
+            </Row>
+            <Row style={{ paddingBottom: '10px' }}>
+              <Text strong>Personal de Administración de Servicios (PAS)%</Text>
+            </Row>
+            <Row style={{ paddingBottom: '10px' }}>
+              <Text strong>Estudiantes (ALU)</Text>
+            </Row>
+          </Col>
+          <Col style={{ width: '50%' }}>
+            <Row style={{ paddingBottom: '10px' }}>
+            <InputNumber  min={1} max={100} defaultValue={1} /> % 
+            </Row>
+            <Row style={{ paddingBottom: '10px' }}>
+            <InputNumber  min={1} max={100} defaultValue={1} /> % 
+            </Row>
+            <Row style={{ paddingBottom: '10px' }}>
+            <InputNumber  min={1} max={100} defaultValue={1} /> %
+            </Row>
+            <Row style={{ paddingBottom: '10px' }}>
+            <InputNumber  min={1} max={100} defaultValue={1} /> %
+            </Row>
+            <Row style={{ paddingBottom: '10px' }}>
+            <InputNumber  min={1} max={100} defaultValue={1} /> %
+            </Row>
+          </Col>
+        </Row></Col>
+    
+        <Card className = "card2">
+                <Text style={{ fontSize: '20px' }}>Duración:</Text>
+        </Card>
+                <RangePicker
+                  disabledDate={comprobarinicio}
+                  onChange={changefecha}
+                  />
+                 <Row>
+                <Button style={{ background: '#206489',color:'#FFFFFF' }}>Cancelar</Button>
+                <Button style={{ background: '#FFA500',color:'#FFFFFF' }}>Crear Elección</Button> </Row>
+             
+             
+          </TabPane>
       </Tabs>
     </body>
+    </Col>
   )
 }
 
--- src/views/Votacion/index.tsx
+++ src/views/Votacion/index.tsx
@@ -1,11 +1,13 @@
-import { Typography, Button, Checkbox, Row, Col, Layout } from "antd";
-import React from "react";
-import { Radio, Input } from "antd";
+import { Typography, Button, Row, Col, Layout } from "antd";
+import React,{useState} from "react";
+import { Input } from "antd";
 import "./index.less";
 import {useOptionsQuery, useVotePollMutation} from "@Generated/hooks"
 import wallpaper from "../../assets/Wallpaper2.png";
 import { useParams } from "react-router-dom";
-import { bool, boolean } from "yup";
+import * as Yup from "yup";
+import {Formik} from 'formik';
+import {Form, Radio, SubmitButton, FormikDebug, Checkbox} from 'formik-antd'
 
 
 const { Text } = Typography;
@@ -25,137 +27,174 @@ type VotePollInput = {
     }
   }
 
-  /*
-const votar = () =>{
-  return (
-    useVotePollMutation(data.VotePollInput);
-  );
-}
-*/
+const schema = Yup.object().shape({
+  validate: Yup.boolean().oneOf([true]),
+  election: Yup.string().required()
+})
 
-const disabled = {
-  state: boolean
+const Votacion = () => {
+  const { id } = useParams<{ id: string }>()
+  const { data, loading } = useOptionsQuery({ variables: { id } })
+  const [validate, setValidate] = useState(false)
+  const [vote] = useVotePollMutation()
+  if (data) {
+    return (
+      <Formik
+        onSubmit={values => {
+          console.log(values)
+          // await vote({variables: { input: { elections: [values.election], poll:id }}})
+        }}
+        initialValues={{ validate: false, election: '' }}
+        initialErrors={{validate:"", election:""}}
+        validateOnBlur={false}
+        validationSchema={schema}
+      >
+        {() => (
+          <>
+            <Form>
+              <Radio.Group
+                name="election"
+                options={data.election.candidates.map(
+                  ({ id, firstName, lastName }) => ({
+                    label: `${firstName} ${lastName}`,
+                    value: id,
+                  })
+                )}
+              />
+              <Checkbox name="validate" />
+              <SubmitButton>Votar</SubmitButton>
+              <FormikDebug />
+            </Form>
+          </>
+        )}
+      </Formik>
+    )
+  }
+  if (loading) {
+    return <div>Cargando...</div>
+  }
+  return <div>Error...</div>
 }
 
-const Votacion = (props: any) => {
-  const {id} = useParams();
-  const {id:poll} = useParams<{id:string}>();
-  const {data} = useOptionsQuery({variables:{id:poll}});
-  const [vote] = useVotePollMutation();
+// const Votacion = (props: any) => {
+//   const {id} = useParams();
+//   const {id:poll} = useParams<{id:string}>();
+//   const {data} = useOptionsQuery({variables:{id:poll}});
+//   const [vote] = useVotePollMutation();
    
-      const radioStyle = {
-        display: "block",
-        height: "30px",
-        lineHeight: "30px"
-      };
+//       const radioStyle = {
+//         display: "block",
+//         height: "30px",
+//         lineHeight: "30px"
+//       };
 
 
-      return (
-        <Row
-          type="flex"
-          justify="center"
-          align="middle"
-          style={{ height: "100%" }}
-        >
-          <Col
-            style={{
-              width: "50%",
-              height: "100%",
-              textAlign: "center"
-            }}
-          >
-            <Layout
-              style={{
-                height: "100%",
-                backgroundImage: "url(" + wallpaper + ")"
-              }}
-            >
+//       return (
+//         <Row
+//           type="flex"
+//           justify="center"
+//           align="middle"
+//           style={{ height: "100%" }}
+//         >
+//           <Col
+//             style={{
+//               width: "50%",
+//               height: "100%",
+//               textAlign: "center"
+//             }}
+//           >
+//             <Layout
+//               style={{
+//                 height: "100%",
+//                 backgroundImage: "url(" + wallpaper + ")"
+//               }}
+//             >
              
-            <Row
-                type="flex"
-                justify="center"
-                //align="middle"
-                style={{
-                  marginTop: "25%"
-                }}
-              > 
-              <Text strong style={{ fontSize: "30px", lineHeight: "100%" }}>
-                  Votación <br></br> {data?.election.description.substring(0, 100-3) + '...'} 
-                </Text>
-              </Row>
+//             <Row
+//                 type="flex"
+//                 justify="center"
+//                 //align="middle"
+//                 style={{
+//                   marginTop: "25%"
+//                 }}
+//               > 
+//               <Text strong style={{ fontSize: "30px", lineHeight: "100%" }}>
+//                   Votación <br></br> {data?.election.description.substring(0, 100-3) + '...'} 
+//                 </Text>
+//               </Row>
         
                
-            </Layout>
-          </Col>
-          <Col
-            style={{
-              width: "50%",
-              height: "100%"
-            }}
-          >
-            <Row
-              type="flex"
-              justify="center"
-              align="middle"
-              style={{ height: "100%" }}
-            >
-              <Radio.Group /*onChange={this.onChange} value={this.state.value hay que poner {props}*/
-              >
-                <Col>
-                  <Text strong style={{ fontSize: "22px" }}>
-                    Su voto:
-                  </Text>
-                  <br></br>
-                  {data?.election.candidates.map((option, index) => (
-                  <Button className="button" style={{ marginTop: "3%" }} /*onClick ={()=>{
-                    vote = {
-                      VotePollInput: {
-                        options: {option.id} ,
-                        poll: {data.id}
-                      }
-                    }
-                  }}*/>
-                    <Row style={{marginTop: "auto"}}>
-                    <Radio style={radioStyle} value={index}>
-                    {option.firstName} {option.lastName}
-                    </Radio></Row>
-                  </Button>))}
+//             </Layout>
+//           </Col>
+//           <Col
+//             style={{
+//               width: "50%",
+//               height: "100%"
+//             }}
+//           >
+//             <Row
+//               type="flex"
+//               justify="center"
+//               align="middle"
+//               style={{ height: "100%" }}
+//             >
+//               <Radio.Group /*onChange={this.onChange} value={this.state.value hay que poner {props}*/
+//               >
+//                 <Col>
+//                   <Text strong style={{ fontSize: "22px" }}>
+//                     Su voto:
+//                   </Text>
+//                   <br></br>
+//                   {data?.election.candidates.map((option, index) => (
+//                   <Button className="button" style={{ marginTop: "3%" }} /*onClick ={()=>{
+//                     vote = {
+//                       VotePollInput: {
+//                         options: {option.id} ,
+//                         poll: {data.id}
+//                       }
+//                     }
+//                   }}*/>
+//                     <Row style={{marginTop: "auto"}}>
+//                     <Radio style={radioStyle} value={index}>
+//                     {option.firstName} {option.lastName}
+//                     </Radio></Row>
+//                   </Button>))}
 
                
-                </Col>
-                <Row
-                  style={{
-                    display: "flex",
-                    alignContent: "space-between",
-                    width: "100%"
-                  }}
-                >
-                  <Button
-                    style={{
-                      backgroundColor: "#206489",
-                      width: "70%",
-                      marginRight: "auto"
-                    }}
-                  >
-                    <Checkbox /*onChange={
+//                 </Col>
+//                 <Row
+//                   style={{
+//                     display: "flex",
+//                     alignContent: "space-between",
+//                     width: "100%"
+//                   }}
+//                 >
+//                   <Button
+//                     style={{
+//                       backgroundColor: "#206489",
+//                       width: "70%",
+//                       marginRight: "auto"
+//                     }}
+//                   >
+//                     <Checkbox /*onChange={
                       
-                      }*/>
-                      <Text style={{ color: "#FFFFFF" }}>
-                        Validar mi elección
-                      </Text>
-                    </Checkbox>
-                  </Button>
+//                       }*/>
+//                       <Text style={{ color: "#FFFFFF" }}>
+//                         Validar mi elección
+//                       </Text>
+//                     </Checkbox>
+//                   </Button>
 
-                  <Button type="primary" /*disabled = {disabled.state}onClick = {() => votar()}*/>
-                    Vota!
-                  </Button>
-                </Row>
-              </Radio.Group>
-            </Row>
-          </Col>
-        </Row>
-      );
-    }
+//                   <Button type="primary" /*disabled = {disabled.state}onClick = {() => votar()}*/>
+//                     Vota!
+//                   </Button>
+//                 </Row>
+//               </Radio.Group>
+//             </Row>
+//           </Col>
+//         </Row>
+//       );
+//     }
   
 
 
