61fb479b608c120d7dda5b92d1d76283a372d1f7
Carmen <dece279@gmail.com>
2020-01-14 15:03:31 +0100

    chore:update

--- src/views/Menu/MenuResultados/index.tsx
+++ src/views/Menu/MenuResultados/index.tsx
@@ -1,18 +1,22 @@
 import { usePastElectionResultsQuery } from "@Generated/hooks";
-import { Row, Typography, Layout } from "antd";
+import { Row, Typography } from "antd";
 import React from "react";
 import ElectionButton from "../Election/election";
 const { Text } = Typography;
 //la interrogacion comprueba que no sea nulo()
 const MenuResultados = () => {
   const data = usePastElectionResultsQuery();
-<<<<<<< HEAD
 
   return (
-    <Row justify="center" className="body">
-      <Row style={{ marginTop: "3%", marginBottom: "1%" }}>
-        <Text strong style={{ fontSize: "20px" }}>
-          Votaciones finalizadas:
+    //<Layout style = {{height: '100%', width: '100%', backgroundColor: '#ffffff', overflow: 'auto'}} >
+    <Row
+      justify="center"
+      className="body"
+      style={{ marginTop: "3%", backgroundColor: "white" }}
+    >
+      <Row className="layout" style={{ marginBottom: "10%" }}>
+        <Text strong style={{ fontSize: "30px" }}>
+          Votaciones finalizadas
         </Text>
       </Row>
       <Row>
@@ -24,35 +28,12 @@ const MenuResultados = () => {
                 dateInit={eprocess.start}
                 dateEnd={eprocess.end}
                 id={eprocess.id}
-                href={"resultados/elections"}
+                href={"resultados"}
               />
             );
           } else {
             return (
               <ElectionButton
-=======
-  
-    return (
-      <Layout style = {{height: '100%', width: '100%', backgroundColor: '#ffffff', overflow: 'auto'}} >
-      <Row justify="center" className="body" style={{ marginTop: '3%', backgroundColor: 'white' }}>
-      <Row className="layout" style={{ marginBottom: '10%' }}>
-        <Text strong style={{ fontSize: "30px" }}>
-          Votaciones finalizadas
-        </Text>
-        </Row>
-        <Row>
-          {data.data?.electoralProcesses.map(eprocess => {
-            if (eprocess.__typename == "Election") {
-              return(<ElectionButton
-              name={eprocess.description}
-              dateInit={eprocess.start}
-              dateEnd={eprocess.end}
-              id={eprocess.id}
-              href={"resultados"}
-              />)
-            }else{
-              return(<ElectionButton
->>>>>>> f29c8cc63db7cf63ed73888c8f136f6b5d9fa061
                 name={eprocess.description}
                 dateInit={eprocess.start}
                 dateEnd={eprocess.end}
@@ -63,13 +44,8 @@ const MenuResultados = () => {
           }
         })}
       </Row>
-<<<<<<< HEAD
-    </Row>
+    </Row> //</Layout>
   );
-=======
-      </Layout>
-    );
->>>>>>> f29c8cc63db7cf63ed73888c8f136f6b5d9fa061
 };
 
 export default MenuResultados;
