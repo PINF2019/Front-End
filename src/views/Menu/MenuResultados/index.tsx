606c815c1fa8cedbf09e834cb3ee1b96fec341a7
Kevin López <kevin.lopezcala99@gmail.com>
2020-01-15 11:07:29 +0100

    Update index.tsx

--- src/views/Menu/MenuResultados/index.tsx
+++ src/views/Menu/MenuResultados/index.tsx
@@ -24,7 +24,7 @@ const MenuResultados = () => {
                 dateInit={eprocess.start}
                 dateEnd={eprocess.end}
                 id={eprocess.id}
-                href="resultados"
+                href="resultados/election"
               />
             )
           }
@@ -34,7 +34,7 @@ const MenuResultados = () => {
               dateInit={eprocess.start}
               dateEnd={eprocess.end}
               id={eprocess.id}
-              href="resultados"
+              href="resultados/poll"
             />
           )
         })}
